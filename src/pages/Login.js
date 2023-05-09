f79c6ba165af55373d596d2187b8d6adec097fc7
yoyoyo8x <ductrung8697@gmail.com>
2023-05-09 15:54:02 +0700

    up

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,6 +23,7 @@ const Login = () => {
       <div className="login-container">
         <img src={Logo} alt="" className="login-logo" />
         <div className="form-container">
+          {/* Social Login */}
           <div className="social-login">
             <button className="github">
               <div className="social-icon">
@@ -30,36 +31,57 @@ const Login = () => {
               </div>
               <span>Gihub</span>
             </button>
-            <button className="gmail">
+            <button className="google">
               <div className="social-icon">
                 <FcGoogle />
               </div>
               <span>Google</span>
             </button>
           </div>
-          <div class="or_container">
-            <div class="line"></div>
+          {/* Break Line */}
+          <div className="line-container">
+            <div className="line"></div>
             <span>OR</span>
             <div class="line"></div>
           </div>
+          {/* Login Form */}
           <form className="login-form" onSubmit={handleSubmit}>
             <div className="form-content">
-              <div>Username: </div>
-              <input
-                type="text"
-                name="Username"
-                value={username}
-                onChange={(e) => setusername(e.target.value)}
-              />
-              <div>Password: </div>
-              <input
-                type="password"
-                name="Password"
-                onChange={(e) => setpassword(e.target.value)}
-              />
+              <div className="input-email">
+                <input
+                  type="text"
+                  name="Email"
+                  placeholder="Email Address"
+                  value={username}
+                  onChange={(e) => setusername(e.target.value)}
+                />
+                <div className="alert">Please enter your email address</div>
+                <div className="invalid">Invalid Email</div>
+              </div>
+              <div className="input-password">
+                <input
+                  type="password"
+                  name="Password"
+                  placeholder="Password"
+                  onChange={(e) => setpassword(e.target.value)}
+                />
+                <div className="alert">Please enter your password</div>
+                <div className="invalid">Wrong password</div>
+              </div>
+              <div className="forgot">Forgot Password?</div>
             </div>
-            <button type="submit">Login</button>
+            <button type="submit" className="submit-btn">
+              Sign in
+            </button>
           </form>
+          <div className="line-container">
+            <div className="line" id="short"></div>
+            <span>Don't have an account?</span>
+            <div class="line" id="short"></div>
+          </div>
+          <button className="submit-btn" id="signup">
+            Sign up
+          </button>
         </div>
       </div>
     </div>
