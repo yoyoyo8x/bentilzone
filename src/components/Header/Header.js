a71abf33c8cee85849efa0c82e1a7899f99bb91b
yoyoyo8x <ductrung8697@gmail.com>
2023-05-18 14:32:34 +0700

    up

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import Logo from "../../images/Logo.png";
 
 const Header = () => {
   return (
-    <header className="tw-bg-lightGray">
+    <header className="tw-bg-lightGray tw-shadow-sm">
       <div className="navbar">
         {/* Navigation logo */}
         <div className="header-logo">
--- src/components/Main/HomeContainer.jsx
+++ src/components/Main/HomeContainer.jsx
@@ -3,13 +3,13 @@ import './Main.css'
 
 function HomeContainer(){
     return(
-        <section id="Home" className="grid-cols-1 md:grid-cols-2 bg-orange-500">
+        <section id="Home" className="tw-grid-cols-1 tw-md:grid-cols-2 tw-bg-orange-500">
             <div className="homeContainer">
-                <div className="homeElement bg-orange-100 rounded-full">
+                <div className="homeElement tw-bg-orange-100 tw-rounded-full">
                     <p>Bike Delivery</p>
                 </div>
             </div>
         </section>
     )
 }
-export default HomeContainer
+export default HomeContainer;
