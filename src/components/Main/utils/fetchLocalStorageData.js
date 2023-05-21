1e392475589210ff67b32d89174f928cee724d85
Dreamlaptrinh <taibooi97@gmail.com>
2023-05-21 20:31:18 +0700

    upError

---
+++ src/components/Main/utils/fetchLocalStorageData.js
@@ -0,0 +1,7 @@
+export const fetchCart = ()=> {
+    const cartInfo = 
+    localStorage.getItem('cartItems')!== "undefined" 
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : localStorage.clear();
+    return cartInfo ? cartInfo : [];
+};
