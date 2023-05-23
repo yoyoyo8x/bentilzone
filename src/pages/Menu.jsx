baab5906e8d2b371949c9fb105af068afd87ab63
Dreamlaptrinh <taibooi97@gmail.com>
2023-05-23 17:32:33 +0700

    updateMenu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import MenuContainer from "../components/Main/MenuContainer"
+import CartContainer from "../components/Header/CartContainer";
+import { useStateValue } from "../components/Context/StateProvider";
+import { useEffect } from "react";
+import { useState } from "react";
+
 
 const Shop = () => {
+  const[{cartShow}, dispatch] = useStateValue()
+  const[scrollValue, setScrollValue] = useState(0)
+  useEffect(() =>{},[scrollValue,cartShow])
+
   return (
-    <div>
-      <div>Menu</div>
-    </div>
+    <main id="Main" className="md:tw-px-16 tw-w-full">
+      <div className="MainContainer">
+      <MenuContainer/>
+      {cartShow && <CartContainer/>}
+      </div>
+    </main>
   );
 };
 
