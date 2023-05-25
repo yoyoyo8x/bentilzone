57d7f83bcfc2793d08482df4edf0c49d84b37d42
yoyoyo8x <ductrung8697@gmail.com>
2023-05-25 20:44:44 +0700

    up

--- src/components/AjaxLoader/AjaxLoader.jsx
+++ src/components/AjaxLoader/AjaxLoader.jsx
@@ -5,7 +5,7 @@ import "../../App.css"
 const Ajax = () => {
   return (
     <div className="tw-w-full tw-h-full tw-flex tw-place-content-center tw-items-center">
-      <img src={Loading} alt="" className="tw-h-40 tw-w-40 tw-mb-48" />
+      <img src={Loading} alt="" className="tw-h-40 tw-w-40" />
     </div>
   );
 };
