5c92de9a6162ff5ffb5ba7ad8454ef5e41801b1e
anhhtthu <124450882+anhhtthu@users.noreply.github.com>
2023-08-18 01:30:40 +0700

    test add token into headers

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,92 +1,92 @@
 import User from "../models/user.js";
 import { signInValid, signUpValid } from "../validation/user.js";
-import bcrypyjs from "bcryptjs"
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
-dotenv.config()
+import bcrypyjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
 
-const {SECRET_CODE} = process.env
+const { SECRET_CODE } = process.env;
 
 export const signUp = async (req, res) => {
-    try {
-        // check validation
-        const {name, email, password} = req.body
-        const {error} = signUpValid.validate(req.body, {abortEarly: true})
-        if(error){
-            const errors = error.details.map(err => err.message)
-            return res.status(400).json({
-                message: errors
-            })
-        }
-        // check email
-        const userExists = await User.findOne({email})
-        if (userExists) {
-            return res.status(400).json({
-                message: "Nguoi dung da duoc dang ky"
-            });
-        }
-        // bcrypyjs password
-        const hashedPassword = await bcrypyjs.hash(password, 10);
-        const user = await User.create({
-            name,
-            email,
-            password: hashedPassword,
-        })
-        // create successfully
-        user.password = undefined;
-        return res.status(200).json({
-        message: "Tao tai khoan thanh cong",
-        user
-    })
-    } catch (error) {
-        return res.status(500).json({
-            message: error
-        })
+  try {
+    // check validation
+    const { name, email, password } = req.body;
+    const { error } = signUpValid.validate(req.body, { abortEarly: true });
+    if (error) {
+      const errors = error.details.map((err) => err.message);
+      return res.status(400).json({
+        message: errors,
+      });
     }
-}
+    // check email
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        message: "Nguoi dung da duoc dang ky",
+      });
+    }
+    // bcrypyjs password
+    const hashedPassword = await bcrypyjs.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    // create successfully
+    user.password = undefined;
+    return res.status(200).json({
+      message: "Tao tai khoan thanh cong",
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+};
 
-export const signIn = async(req,res)=>{
-    try {
-        // check validation
-        const{email, password} = req.body
-        const{error} = signInValid.validate(req.body, {abortEarly:true})
-        if(error){
-            const errors = error.details.map((err) => err.message)
-            return res.status(500).json({
-                message: errors
-            })
-        }
-        // check user
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(400).json({
-                message: "Khong tim thay nguoi dung"
-            })
-        }
-        // check pass
-        const isMatch = await bcrypyjs.compare(password, user.password);
-        if(!isMatch){
-            return res.status(400).json({
-                message:"Mat khau khong dung"
-            })
-        }
-        // create jwt
-        const token = jwt.sign(
-            {id: user.id},
-            SECRET_CODE,
-            {expiresIn: "1d"}
-        )
-        console.log(token)
-        // access successfully
-        user.password = undefined
-        return res.status(200).json({
-            message:"Dang nhap thanh cong",
-            user: user,
-            accessToken: token
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: error
-        })
+export const signIn = async (req, res) => {
+  try {
+    // check validation
+    const { email, password } = req.body;
+    const { error } = signInValid.validate(req.body, { abortEarly: true });
+    if (error) {
+      const errors = error.details.map((err) => err.message);
+      return res.status(500).json({
+        message: errors,
+      });
     }
-}
+    // check user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Khong tim thay nguoi dung",
+      });
+    }
+    // check pass
+    const isMatch = await bcrypyjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Mat khau khong dung",
+      });
+    }
+    // create jwt
+    const token = jwt.sign({ id: user.id }, SECRET_CODE, { expiresIn: "1d" });
+    console.log(token);
+    axios.defaults.headers.common = {
+      Authorization: "Bearer " + token,
+    };
+    // access successfully
+    user.password = undefined;
+    return res.status(200).json({
+      message: "Dang nhap thanh cong",
+      user: user,
+      accessToken: token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+};
--- backend/src/middlewares/checkPermission.js
+++ backend/src/middlewares/checkPermission.js
@@ -1,35 +1,35 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import User from "../models/user.js";
-dotenv.config()
+dotenv.config();
 
-const {SECRET_CODE} = process.env;
+const { SECRET_CODE } = process.env;
 
 export const checkPermission = async (req, res, next) => {
-    try {
-        // find token from headers
-        const bearToken = req.headers.authorization
-        if (!bearToken){
-            throw new Error("Bạn chưa đăng nhập")
-        }
-        const token = req.headers.authorization.split(" ")[1];
-        if (!token){
-            throw new Error("Token rỗng!")   
-        }
-        // decoded token
-        const decoded = jwt.verify(token, SECRET_CODE)
-        // find user from token.payload that decoded
-        const user = await User.findById(decoded.id)
-
-        // check permission
-        if(!user || user.role !== "admin"){
-            throw new Error(" Ban khong duoc cap quyen")
-        }
-        // you can do it
-        next()
-    } catch (error) {
-        return res.status(401).json({
-            message: error.message || "Bạn không có quyền"
-        })
+  try {
+    // find token from headers
+    const bearToken = req.headers.authorization;
+    if (!bearToken) {
+      throw new Error("Bạn chưa đăng nhập");
     }
-}
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("Token rỗng!");
+    }
+    // decoded token
+    const decoded = jwt.verify(token, SECRET_CODE);
+    // find user from token.payload that decoded
+    const user = await User.findById(decoded.id);
+
+    // check permission
+    if (!user || user.role !== "admin") {
+      throw new Error(" Ban khong duoc cap quyen");
+    }
+    // you can do it
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: error.message || "Bạn không có quyền",
+    });
+  }
+};
